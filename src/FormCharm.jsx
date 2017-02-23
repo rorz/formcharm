00fae183307f0fa3d366a90a5262a3d767a5461a
rorz <rory.mcmeekin@me.com>
2017-02-23 15:46:06 +0000

    added basic prop types and model outline

--- src/FormCharm.jsx
+++ src/FormCharm.jsx
@@ -3,13 +3,27 @@ import React from 'react'
 export default class FormCharm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-
-        }
+        this.state = {}
     }
+
+    submit() {
+
+    }
+
     render() {
         return (
-            
+            <div onClick={() => this.submit()} style={this.props.style} data={this.props.data} replyTo={this.props.replyTo || true} sendCopy={this.props.sendCopy || false} inbox={this.props.inbox} formName={this.props.formName}>
+                {this.props.children}
+            </div>
         )
     }
+}
+
+FormCharm.propTypes = {
+    style: React.PropTypes.object,
+    data: React.PropTypes.object,
+    replyTo: React.PropTypes.boolean,
+    sendCopy: React.PropTypes.boolean,
+    inbox: React.PropTypes.string,
+    formName: React.PropTypes.string
 }
