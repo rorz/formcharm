0e9a318e51eb68352a3a2b612ff8101cb6c83b43
rorz <rory.mcmeekin@me.com>
2017-02-27 11:41:09 +0100

    added commentary for the props

--- src/FormCharm.jsx
+++ src/FormCharm.jsx
@@ -8,7 +8,7 @@ export default class FormCharm extends React.Component {
         const submissionData = {
             data: this.props.data,
             replyTo: this.props.replyTo,
-            sendCopy: this.props.sendCopy,
+            sendCopies: this.props.sendCopies,
             formName: this.props.formName,
             inbox: this.props.inbox
         }
@@ -46,17 +46,23 @@ export default class FormCharm extends React.Component {
 }
 
 FormCharm.propTypes = {
-    callback: React.PropTypes.func,
+    // Event Handling:
+    callback: React.PropTypes.func, // Callback upon request returning
 
-    style: React.PropTypes.object,
-    className: React.PropTypes.string,
+    // Styling:
+    style: React.PropTypes.object, // Style props passed to the wrapper
+    className: React.PropTypes.string, // Classname prop passed to the wrapper
 
-    inbox: React.PropTypes.string,
-    formName: React.PropTypes.string,
-    sendCopies: React.PropTypes.object,
-    replyTo: React.PropTypes.bool,
+    // Admin-Side:
+    inbox: React.PropTypes.string.isRequired, // Email address to receive submissions to
+    formName: React.PropTypes.string, // PRO FEATURE — Name of this particular form instance (as to appear in subject line of submission)
+    sendCopies: React.PropTypes.array, // PRO FEATURE — An array of email values to CC to the email: i.e. ['copy@example.com', 'copy-2@example.com']
+    replyTo: React.PropTypes.bool, // PRO FEATURE — Sets 'reply-to' in the email header so that you can reply directly to the submitter from your email client (SUBMITTER prop REQUIRED)
 
-    submitter: React.PropTypes.string,
-    data: React.PropTypes.object,
-    sendConfirmation: React.PropTypes.bool
+    // Form Data:
+    submitter: React.PropTypes.string, // Not required but, highly recommended (for your own sake)
+    data: React.PropTypes.object, // Contains all the custom form data
+
+    // Submitter-Side:
+    sendConfirmation: React.PropTypes.bool // PRO FEATURE — Sends a confirmation summary to the submitter (SUBMITTER prop REQUIRED)
 }
